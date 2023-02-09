eff20c975fbeb0f44b5779779f98a76577b927ed
Renan Blasechi <renan@fasters.com.br>
2023-02-09 00:51:06 -0300

    implementacao de testes unitarios

--- Autoglass.Api/StartupConfig/DependencyInjectConfig.cs
+++ Autoglass.Api/StartupConfig/DependencyInjectConfig.cs
@@ -2,8 +2,6 @@
 using Autoglass.Domain.Entities;
 using Autoglass.Domain.Repository.Interfaces;
 using Autoglass.Repository.Implementations;
-using Autoglass.Service.Implementations;
-using Autoglass.Service.Repository.Interfaces;
 using Autoglass.Service.Service.Implementations;
 using Autoglass.Service.Service.Interfaces;
 using AutoMapper;
@@ -30,7 +28,6 @@ namespace Autoglass.Api.StartupConfig
 
             services.AddTransient<IProductRepository, ProductRepository>();
             services.AddTransient<IProviderRepository, ProviderRepository>();
-            //services.AddTransient(typeof(IServiceBase<>));
             services.AddTransient<IProductService, ProductService>();
             services.AddTransient<IProviderService, ProviderService>();
         }
